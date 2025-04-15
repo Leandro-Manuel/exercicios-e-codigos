d875bfcd17ffdc7747f3b8eccbfb813eb4d4e783
Leandro Manuel Francisco <leandro_00010@outlook.com>
2025-04-14 23:17:08 -0300

    primeiro-exercicio

---
+++ exercicio1.py
@@ -0,0 +1,10 @@
+# receba dois numeros e faça a soma
+
+nmr1 = int(input('Escreva um número maior que 0: '))
+nmr2 = int(input('Escreva outro número maior que 0: '))
+
+if nmr1 > 0 and nmr2 >0:
+    soma = nmr1 + nmr2
+    print('A soma dos dois números é: ',soma)
+else:
+    print('O número deve ser maior que 0')
