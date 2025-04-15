d848540d5765996e079f6e9783622b8ad282e8d2
Leandro Manuel Francisco <leandro_00010@outlook.com>
2025-04-14 23:38:35 -0300

    ab

--- exercicio1.py
+++ exercicio1.py
@@ -1,5 +1,4 @@
 # receba dois numeros e faça a soma
-from pip._internal.vcs import git
 
 nmr1 = int(input('Escreva um número maior que 0: '))
 nmr2 = int(input('Escreva outro número maior que 0: '))
@@ -8,4 +7,4 @@ if nmr1 > 0 and nmr2 >0:
     soma = nmr1 + nmr2
     print('A soma dos dois números é: ',soma)
 else:
-    print('O número deve ser maior que 0')git
+    print('O número deve ser maior que 0')
