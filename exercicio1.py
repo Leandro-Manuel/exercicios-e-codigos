42e4c85e646b627bafcb92e23a152044ffe775c6
Leandro Manuel Francisco <leandro_00010@outlook.com>
2025-04-15 00:00:14 -0300

    exercicio1 finalizado

--- exercicio1.py
+++ exercicio1.py
@@ -1,10 +1,29 @@
-# receba dois numeros e faça a soma
+# receba dois numeros e faça as quatro operações aritméticas de acordo com a preferencia do usuario
 
-nmr1 = int(input('Escreva um número maior que 0: '))
-nmr2 = int(input('Escreva outro número maior que 0: '))
+print('Escolha uma das opções: ')
+print('1 - Somar dois números')
+print('2 - Subtrair dois números')
+print('3 - multiplicar dois números')
+print('4 - dividir dois números')
 
-if nmr1 > 0 and nmr2 >0:
-    soma = nmr1 + nmr2
-    print('A soma dos dois números é: ',soma)
-else:
-    print('O número deve ser maior que 0')
+escolha = int(input('Faça a sua escolha: '))
+
+if escolha>=1 and escolha<=4:
+    nmr1 = int(input('Escreva um número maior que 0: '))
+    nmr2 = int(input('Escreva outro número maior que 0: '))
+    if nmr1 > 0 and nmr2 > 0:
+        if escolha == 1:
+            resultado = nmr1 + nmr2
+            print('A soma dos dois números é:',resultado)
+        elif escolha == 2:
+            resultado = nmr1 - nmr2
+            print('A soma dos dois números é:', resultado)
+        elif escolha == 3:
+            resultado = nmr1 * nmr2
+            print('A soma dos dois números é:', resultado)
+        else:
+            resultado = nmr1 / nmr2
+            print('A soma dos dois números é:', resultado)
+    else:
+        print('O número deve ser maior que 0')
+else: print('Escolha entre 1 e 4')
