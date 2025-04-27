d9ed584d63d4b2c8dcd340585936d9b4c69b9fe0
Leandro Manuel Francisco <leandro_00010@outlook.com>
2025-04-26 22:28:19 -0300

    tratamento de exceção concluída

--- caixaEletronico.py
+++ caixaEletronico.py
@@ -18,13 +18,16 @@ while (verificacao):
         confirma_escolha = str(input('1 - Saque, confirma? (S/N): '))
         if (confirma_escolha in 'Ss'):
             valor_saque = float(input('Digite o valor para saque: '))
-            if (valor_saque != 0 and valor_saque > 0):
+            if (valor_saque != 0 and valor_saque > 0 and conta_usuario >= valor_saque):
                 conta_usuario = conta_usuario - valor_saque
                 print(f'Saque realizado no valor de {valor_saque} reais, saldo disponível {conta_usuario} reais.')
             else:
                 while (valor_saque > conta_usuario):
                     print(f'Não é possível realizar a operação, seu saldo atual é de {conta_usuario} reais.')
                     valor_saque = float(input('Digite novamente o valor para saque: '))
+                if (valor_saque != 0 and valor_saque > 0 and conta_usuario >= valor_saque):
+                    conta_usuario = conta_usuario - valor_saque
+                    print(f'Saque realizado no valor de {valor_saque} reais, saldo disponível {conta_usuario} reais.')
         else:
             break
 
@@ -39,6 +42,9 @@ while (verificacao):
                 while(valor_deposito <= 0):
                     print('Valor inválido, insira uma quantidade maior que 0.')
                     valor_deposito = float(input('Informe novamente: '))
+                if (valor_deposito != 0 and valor_deposito > 0):
+                    conta_usuario = conta_usuario + valor_deposito
+                    print(f'Operação realizada, você depositou {valor_deposito} reais. Seu saldo atual é {conta_usuario} reais.')
 
     elif (escolha_menu == 3):
         confirma_escolha = str(input('3 - mostrar saldo atual, confirma? (S/N): '))
