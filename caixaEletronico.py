7ade14b03fc7ca1b4094b002f610a93fdb97a8a5
Leandro Manuel Francisco <leandro_00010@outlook.com>
2025-04-28 22:22:36 -0300

    alterações no código

--- caixaEletronico.py
+++ caixaEletronico.py
@@ -5,8 +5,8 @@ import sys
 conta_usuario = 0
 verificacao = True
 
-historico_saque = []
-historico_deposito = []
+historico_transacao = []
+
 contagem_historico = 0
 
 while (verificacao):
@@ -23,16 +23,19 @@ while (verificacao):
                 conta_usuario -= valor_saque
                 contagem_historico += 1
                 print(f'Saque realizado no valor de {valor_saque} reais, saldo disponível R$ {conta_usuario}')
-                historico_saque += [contagem_historico,'saque',valor_saque,'reais']
+                historico_transacao += [contagem_historico,'saque',valor_saque,'reais']
             else:
                 while (valor_saque > conta_usuario):
                     print(f'Não é possível realizar a operação, seu saldo atual é de R$ {conta_usuario}')
                     valor_saque = float(input('Digite novamente o valor para saque: '))
-                if (valor_saque > 0 and conta_usuario >= valor_saque):
+                if (valor_saque > 0):
                     conta_usuario -= valor_saque
                     contagem_historico += 1
                     print(f'Saque realizado no valor de {valor_saque} reais, saldo disponível R$ {conta_usuario} .')
-                    historico_saque += [contagem_historico,'saque', valor_saque, 'reais']
+                    historico_transacao += [contagem_historico,'saque', valor_saque, 'reais']
+                else:
+                    print('Não foi possível completar sua operação, tente novamente.')
+                    sys.exit()
         else:
             print('Encerrando o programa...')
             sys.exit()
@@ -45,7 +48,7 @@ while (verificacao):
                 conta_usuario += valor_deposito
                 contagem_historico += 1
                 print(f'Operação realizada, você depositou R$ {valor_deposito}. Seu saldo atual é R$ {conta_usuario}')
-                historico_deposito += [contagem_historico,'deposito',valor_deposito, 'reais']
+                historico_transacao += [contagem_historico,'deposito',valor_deposito, 'reais']
             else:
                 while(valor_deposito <= 0):
                     print('Valor inválido, insira uma quantidade maior que 0.')
@@ -54,12 +57,16 @@ while (verificacao):
                     conta_usuario += valor_deposito
                     contagem_historico += 1
                     print(f'Operação realizada, você depositou {valor_deposito} reais. Seu saldo atual é R$ {conta_usuario}')
-                    historico_deposito += [contagem_historico,'deposito', valor_deposito, 'reais']
+                    historico_transacao += [contagem_historico,'deposito', valor_deposito, 'reais']
 
     elif (escolha_menu == 3):
         confirma_escolha = str(input('3 - mostrar saldo atual, confirma? (S/N): '))
         if (confirma_escolha in 'Ss'):
-            print(f'Seu saldo atual é {conta_usuario} reais.')
+            if (historico_transacao):
+                print(f'Seu saldo atual é {conta_usuario} reais.')
+            else:
+                print('Não há nenhum registro.')
+                sys.exit()
         else:
             print('Encerrando o programa...')
             sys.exit()
@@ -67,25 +74,22 @@ while (verificacao):
     elif (escolha_menu == 4):
         confirma_escolha_quatro = str(input('4 - Consultar histórico, confirma? (S/N): '))
         if (confirma_escolha_quatro in 'Ss'):
-            if historico_deposito:
-                print(historico_deposito)
+            if historico_transacao:
+                print(historico_transacao)
             else:
-                print('Não há registro de saque')
-
-            if historico_saque:
-                print(historico_saque)
-            else:
-                print('Não há registro de deposito')
+                print('Não há nenhum registro')
+                sys.exit()
         else:
             print('Encerrando o programa...')
             sys.exit()
     else:
-        print('Opção inválida')
+        print('Opção inválida, encerrando o programa...')
         sys.exit()
 
     confirma_escolha = str(input('Deseja realizar outra operação?(S/N): '))
     if (confirma_escolha in 'Ss'):
         verificacao = True
     else:
+        print('Encerrando o programa...')
         verificacao = False
 
